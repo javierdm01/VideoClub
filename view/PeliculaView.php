8051be70da6c60d83ea890f4549b881d66f121c8
javierdm01 <diazmolinajavier04@gmail.com>
2023-12-25 22:48:44 +0100

    Modificar Peliculas

--- view/PeliculaView.php
+++ view/PeliculaView.php
@@ -24,7 +24,7 @@ class PeliculaView {
     }
     public function mostrarAcciones($id){
         echo '<td><form method="POST" action="' . $_SERVER["PHP_SELF"] . '">
-                        <input type="hidden" name="mod" value="' . $id . '">
+                        <input type="hidden" name="mod" value="' . $id-1 . '">
                         <button class="btn btn-primary border" type="submit"><i class="fa-solid fa-pencil"></i></button>
                     </form></td>';
                 echo '<td><form method="POST" action="' . $_SERVER["PHP_SELF"] . '">
@@ -36,23 +36,35 @@ class PeliculaView {
     
     public function mostrarPeliculas(&$mod,$peliculas,$i) {
         
-            if ($mod === $i) {
-                echo '<tr><form method="POST" action="' . $_SERVER["PHP_SELF"] . '">';
+            if ($mod!=null && intval($mod) === $i) {
+                echo ''
+                . '<form method="POST" action="' . $_SERVER["PHP_SELF"] . '"><tr>';
                 echo '<input type="hidden" id="datos" name="datos" value="">';
                 echo '<input type="hidden" name="id" value="' . $peliculas[$i]['id'] . '">';
-                echo '
-                <td class="w-25 ">
-                    <img class="w-50 mx-auto img-fluid" src="../assets/img/' . $peliculas[$i]['cartel'] . '">
-                    <input value=' . $peliculas[$i]['cartel'] . ' type="text" name="cartel" class="form-control" id="cartel">
-                </td>
-                <td class="">
-                    <input value="' . $peliculas[$i]['titulo'] . '" type="text" name="titulo"  class="form-control my-3" id="titulo" placeholder="Ejemplo: Titanic" required>
-                    <input value="' . $peliculas[$i]['genero'] . '" type="text" name="genero"  class="form-control my-3" id="genero" placeholder="Ejemplo: Terror" required>
-                    <input value="' . $peliculas[$i]['pais'] . '" type="text" name="pais"  class="form-control my-3" id="pais" placeholder="Ejemplo: España" required>
-                    <input value="' . $peliculas[$i]['anyo'] . '" type="number" name="anyo"  class="form-control my-3" id="anyo" placeholder="Ejemplo: 2023" required>
-                   </td>';
-                echo '<td><button class="btn btn-primary border" type="submit">Modificar Tabla</button></td>';
-                echo '</form></tr>';
+
+                echo '<td class="w-25">';
+                echo '<img class="w-50 mx-auto img-fluid" src="../assets/img/' . $peliculas[$i]['cartel'] . '">';
+                echo '<input value=' . $peliculas[$i]['cartel'] . ' type="text" name="cartel" class="form-control" id="cartel">';
+                echo '</td>';
+
+                echo '<td class="w-25">';   
+                echo '<input value="' . $peliculas[$i]['titulo'] . '" type="text" name="titulo"  class="form-control my-3" id="titulo" placeholder="Ejemplo: Titanic" required>';
+                echo '</td>';
+
+                echo '<td class="w-25">';
+                echo '<input value="' . $peliculas[$i]['genero'] . '" type="text" name="genero"  class="form-control my-3" id="genero" placeholder="Ejemplo: Terror" required>';
+                echo '</td>';
+
+                echo '<td class="w-25">';
+                echo '<input value="' . $peliculas[$i]['pais'] . '" type="text" name="pais"  class="form-control my-3" id="pais" placeholder="Ejemplo: España" required>';
+                echo '</td>';
+
+                echo '<td colspan="2" class="w-25">';
+                echo '<input value="' . $peliculas[$i]['anyo'] . '" type="number" name="anyo"  class="form-control my-3" id="anyo" placeholder="Ejemplo: 2023" required>';
+                echo '</td>';
+                $this->modalModificar();                
+                echo '<td colspan="2"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#miModal">Modificar Peliculas</button>';
+                echo '</td></tr>';
             } else {
                 echo '<tr>
                     <td class="w-25"><img class="w-50 mx-auto img-fluid" src="../assets/img/' . $peliculas[$i]['cartel'] . '"></td>
@@ -118,6 +130,31 @@ class PeliculaView {
                 </div>
             </div>
         </div>';
+    }
+    
+    public function modalModificar(){
+        echo '<div class="modal" id="miModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modificar Pelicula</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Agregar Nuevo coche-->
+                            <h2>¿Estás seguro que quieres modificar esta pelicula?</h2>
+
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                <input type="submit" class="btn btn-primary" value="Guardar">
+                            </div>
+                    </div>
+                </div>
+                </div>
+        </div></form>';
     }
 }
    
